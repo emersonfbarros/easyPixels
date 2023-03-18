4a323f3de901511617576d40479ce74839cf426b
emersonfbarros <emersonfb99@gmail.com>
2023-03-18 12:01:26 -0300

    adicionada segunda cor fixa como branco para ser usado de borracha

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Captured element
+let dimension = 5; // Usado nos requisitos 6, 13, 14 e 15;
+const buttonColors = document.getElementById('button-random-color');
+const board = document.getElementById('pixel-board');
+const colors = []; // guardará os elementos html das 4 cores da paleta e é globalmente acessável
+const clearBtn = document.getElementById('clear-board');
+const generateBtn = document.getElementById('generate-board');
+
 function randomColor() { // gera as cores rgb aleatoriamente
   const rdmColor = [];
   for (let colorIndex = 0; colorIndex < 3; colorIndex += 1) { // cria aleatoriamente a cor rgb, usei como referência esse artigo https://www.rustcodeweb.com/2021/07/how-to-generate-random-rgb-color-using-javascript.html
@@ -9,58 +17,50 @@ function randomColor() { // gera as cores rgb aleatoriamente
   return `rgb(${rdmColor[0]}, ${rdmColor[1]}, ${rdmColor[2]})`;
 }
 
-const selectedColor = (color) => (color === 'black' ? 'selected' : 'color');
-
-// Requisito 2 feito através da manipulação do DOM
-const colors = []; // guardará os elementos html das 4 cores da paleta e é globalmente acessável
-
 function createPallet() {
   const colorsPallete = document.getElementById('color-palette'); // captura a sections que terá a palte
-  for (let index = 0; index < 4; index += 1) { // for que irá criar os elementos span que serão as cores da paleta
+  for (let index = 0; index < 5; index += 1) { // for que irá criar os elementos span que serão as cores da paleta
     colors[index] = document.createElement('div'); // cria o elemento span
-    colors[index].style.backgroundColor = (index === 0) ? 'black' : randomColor(); // Requisito 3: primeira paleta fixa na cor preta e as demais cores aleatórias;
     colors[index].classList.add('color'); // adiciona a classe 'color'
-    colors[index].classList.add(selectedColor(colors[index].style.backgroundColor)); // adiciona a classe 'color'
+    if (index === 0) {
+      colors[index].style.backgroundColor = 'black';
+      colors[index].classList.add('selected')
+    } else if (index === 1) {
+      colors[index].style.backgroundColor = 'white';
+    } else {
+      colors[index].style.backgroundColor = randomColor();
+    }
     colorsPallete.appendChild(colors[index]);
   }
 }
 
-// Requisito 4
-const buttonColors = document.getElementById('button-random-color');
-
 function recreatePallet() {
-  for (let remakeIndex = 1; remakeIndex < colors.length; remakeIndex += 1) {
+  for (let remakeIndex = 2; remakeIndex < colors.length; remakeIndex += 1) {
     colors[remakeIndex].style.backgroundColor = randomColor();
   }
 }
 
-// Requisito 5
 function storageColors() {
   const savedColors = [];
-  for (let storageIndex = 0; storageIndex < 4; storageIndex += 1) {
+  for (let storageIndex = 0; storageIndex < 5; storageIndex += 1) {
     savedColors[storageIndex] = colors[storageIndex].style.backgroundColor;
   }
   localStorage.setItem('colorPalette', JSON.stringify(savedColors));
 }
 
-function setColors() {
+function recoverColors() {
   const recoveredColors = JSON.parse(localStorage.getItem('colorPalette'));
-  for (let setIndex = 0; setIndex < 4; setIndex += 1) {
+  for (let setIndex = 0; setIndex < 5; setIndex += 1) {
     colors[setIndex].style.backgroundColor = recoveredColors[setIndex];
   }
 }
 
-let dimension = 5; // Usado nos requisitos 6, 13, 14 e 15;
-
-// Requisito 15 - parte 2
 function restoreBoard() {
   if (localStorage.getItem('boardSize') !== null) {
     dimension = JSON.parse(localStorage.getItem('boardSize'));
   }
 }
 
-// Requisito 6
-const board = document.getElementById('pixel-board');
 function createBoard() {
   restoreBoard();
   const columns = [];
@@ -70,6 +70,7 @@ function createBoard() {
       columns[columIndex] = document.createElement('div');
       columns[columIndex].classList = 'pixel';
       columns[columIndex].style.backgroundColor = 'white';
+      columns[columIndex].style.border = '1px solid rgba(0, 0, 0, 0.4)'
       board.appendChild(columns[columIndex]);
     }
     gridColums += 'auto ';
@@ -77,23 +78,20 @@ function createBoard() {
   board.style.gridTemplateColumns = gridColums;
 }
 
-// Requisito 9
-const newSelected = document.getElementsByClassName('color');
+
 function selectedOne(event) {
   const initialSelected = document.querySelector('.selected');
   initialSelected.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
-// Requisito 10
-const pixelToColor = document.getElementsByClassName('pixel');
 function coloredPixel(event) {
   const pixelToFill = event;
   const colorToFill = document.querySelector('.selected').style.backgroundColor;
+  pixelToFill.target.style.border = (colorToFill !== 'white') ? '1px solid transparent' : '1px solid rgba(0, 0, 0, 0.4)';
   pixelToFill.target.style.backgroundColor = colorToFill;
 }
 
-// Requisito 12
 function saveArt() {
   const savedArt = [];
   for (let artIndex = 0; artIndex < pixelToColor.length; artIndex += 1) {
@@ -109,36 +107,31 @@ function recoverArt() {
   }
 }
 
-// Requisito 11
-const clearBtn = document.getElementById('clear-board');
 function clearPixels() {
   for (let clearIndex = 0; clearIndex < pixelToColor.length; clearIndex += 1) {
     pixelToColor[clearIndex].style.backgroundColor = 'white';
+    pixelToColor[clearIndex].style.border = '1px solid rgba(0, 0, 0, 0.4)';
   }
   saveArt();
 }
 
-// Requisito 14
 function sizeLimiter() {
-  if (dimension < 5) {
-    dimension = 5;
+  if (dimension < 2) {
+    dimension = 2;
   }
   if (dimension > 50) {
     dimension = 50;
   }
 }
 
-// Requisito 15 - parte 1
 function saveBoard() {
   JSON.stringify(localStorage.setItem('boardSize', dimension));
 }
 
-// Requisito 13
-const generateBtn = document.getElementById('generate-board');
 function newBoard() {
   const sizeInput = parseInt(document.getElementById('board-size').value, 10); // Para o requisito 13
   if (Number.isNaN(sizeInput)) {
-    window.alert('Board inválido!');
+    window.alert('Invalid Board!');
     return;
   }
   dimension = sizeInput;
@@ -155,13 +148,17 @@ function strictlyOnload() {
   if (localStorage.getItem('colorPalette') === null) {
     storageColors();
   }
-  setColors();
+  recoverColors();
   createBoard();
   if (localStorage.getItem('pixelBoard') === null) {
     saveArt();
   }
   recoverArt();
 }
+
+//Captures for the events function
+const newSelected = document.getElementsByClassName('color');
+const pixelToColor = document.getElementsByClassName('pixel');
 
 function events() {
   buttonColors.addEventListener('click', recreatePallet);
